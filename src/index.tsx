a2c19ce84e37645d5a2871c169be7ed016a878be
Matheus Couto <couto.matheus@outlook.com>
2018-08-26 18:59:01 -0300

    config: add store and router to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+// CSS IMPORTS
+import './App.css';
+import './index.css';
+
+import store from './store'
+
+
 ReactDOM.render(
-  <App />,
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
