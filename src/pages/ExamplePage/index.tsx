5afa5670dc226c89fea097b281edd8fae50f2836
Matheus Couto <couto.matheus@outlook.com>
2018-08-26 19:01:11 -0300

    feat: create initial router in App file

---
+++ src/pages/ExamplePage/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+import logo from '../../logo.svg';
+
+class ExamplePage extends React.PureComponent {
+	public render() {
+		return(
+			<div>
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.tsx</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
+}
+
+export default ExamplePage;
