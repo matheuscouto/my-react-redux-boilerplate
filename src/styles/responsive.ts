32d3802c4ae0f8d400c3af1ae84ceb33c77ecc56
Matheus Couto <couto.matheus@outlook.com>
2018-11-27 19:57:00 -0200

    feat: responsive style-set

---
+++ src/styles/responsive.ts
@@ -0,0 +1,12 @@
+
+export const breakpoints = {
+  large: 1030,
+  medium: 850,
+  phone: 450
+};
+
+export const smaller = (width: number) => `@media screen and (max-width: ${width}px)`;
+export const smallerHeight = (height: number) => `@media screen and (max-height: ${height}px)`;
+export const widerThan = (width: number) => `@media screen and (min-width: ${width}px)`;
+
+export const isHorizontal = smaller(769);
